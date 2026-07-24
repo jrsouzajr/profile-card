cb962b18c358bba32644bfdb0c18aecd501c6205
Jose Souza <jrsouzajr@yahoo.com>
2026-07-23 21:45:46 -0400

    TODO - Fixing some stuffs

---
+++ src/components/text-input-field/text-input-field.tsx
@@ -0,0 +1,11 @@
+// TODO: Transform the inputs from 'modal-edit-profile' in component here with validation and callbacks.
+
+const TextInputField = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default TextInputField
